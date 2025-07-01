9b4f2caf79e8dba6eb5ffa3ffaada14dea6b5333
Aneesh Gangurde <113239891+Fu11m3tall@users.noreply.github.com>
2025-07-01 12:16:24 +0530

    update- boilerplates' packages

--- BRDtoApp/boilerplate_templates/react_vite_ts/vite.config.ts
+++ BRDtoApp/boilerplate_templates/react_vite_ts/vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3001',
